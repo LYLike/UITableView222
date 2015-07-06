c0662fdbe589cb46d09a912cbf8958c9f605a9bd
qianfeng001 <qianfeng001@zyg.local>
2015-07-06 09:08:52 +0800

    Initial Commit

---
+++ UITableView2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableView2
+//
+//  Created by qianfeng001 on 15/7/6.
+//  Copyright (c) 2015年 zyg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
